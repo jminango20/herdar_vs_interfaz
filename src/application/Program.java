41ff38ede4fefbea3be3e09aab72fd7d70b38484
jminango20 <jcarlosminango@gmail.com>
2021-02-22 15:43:21 -0300

    Uso de interfaz e classe abstrata

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,7 @@
 package application;
 
+import model.entities.AbstractShape;
 import model.entities.Circle;
-import model.entities.Shape;
 import model.enums.Color;
 
 public class Program {
@@ -9,8 +9,8 @@ public class Program {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Shape s1 = new Circle(Color.WHITE, 2.0);
-		Shape s2 = new model.entities.Rectangle(Color.BLACK, 3.0, 4.0);
+		AbstractShape s1 = new Circle(Color.WHITE, 2.0);
+		AbstractShape s2 = new model.entities.Rectangle(Color.BLACK, 3.0, 4.0);
 		
 		System.out.println("Circle color: " + s1.getColor());
 		System.out.println("Circle area: " + String.format("%.3f", s1.area()));
--- src/model/entities/Shape.java
+++ src/model/entities/Shape.java
@@ -1,26 +1,8 @@
 package model.entities;
 
-import model.enums.Color;
-
-public abstract class Shape {
+public interface Shape {
 	
-	private Color color;
-
-	public Shape() {
-	}
-	
-	public Shape(Color color) {
-		this.color = color;
-	}
-
-	public Color getColor() {
-		return color;
-	}
-
-	public void setColor(Color color) {
-		this.color = color;
-	}
-	
-	public abstract double area();
+	//Definicion del metodo
+	double area();
 
 }
